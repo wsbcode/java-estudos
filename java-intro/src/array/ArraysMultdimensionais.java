ab9e3858f5081ecb0c28e2105bad79ab78c05f15
William Barbosa <wsbprofissional@gmail.com>
2026-07-24 20:06:31 -0300

    Arrays JAVA

---
+++ java-intro/src/array/ArraysMultdimensionais.java
@@ -0,0 +1,12 @@
+package array;
+
+public class ArraysMultdimensionais {
+    public static void main(String[] args) {
+    int [][] dias = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+    };
+        System.out.println(dias[0][0]);
+    }
+}
