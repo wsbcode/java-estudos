997bb86e86c4e04d4a807712dafcb8533fdc4a18
William Barbosa <wsbprofissional@gmail.com>
2026-08-10 22:09:13 -0300

    Construtores

---
+++ java-intro/src/java03objeto/construtores/Construtores.java
@@ -0,0 +1,76 @@
+package java03objeto.construtores;
+
+public class Construtores {
+    // // Atributos privados
+    private String nome;
+    private int idade;
+    private char sexo;
+    private int dataNascimento;
+    private String dataCriacao;
+
+    // // Construtor 1: recebe os 3 dados básicos
+    public Construtores(String nome, int idade, char sexo) {
+        this();
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    // // Construtor 2: sobrecarga que chama o Construtor 1 e adiciona o ano de nascimento
+    public Construtores(String nome, int idade, char sexo, int dataNascimento) {
+        this(nome, idade, sexo);
+        this.dataNascimento = dataNascimento;
+    }
+    // // Construtor Vazio: define uma regra padrão para QUALQUER usuário
+    public Construtores() {
+        this.dataCriacao = "10/08/2026"; // // Preenche a data atual automaticamente
+        System.out.println("LOG: Novo usuário instanciado no sistema.");
+    }
+    // // Método para exibir os dados no console
+    public void imprimir() {
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(sexo);
+        System.out.println(dataNascimento);
+    }
+
+    // // Getter do Nome
+    public String getNome() {
+        return nome;
+    }
+
+    // // Setter do Nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // // Getter da Idade
+    public int getIdade() {
+        return idade;
+    }
+
+    // // Setter da Idade
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    // // Getter do Sexo
+    public char getSexo() {
+        return sexo;
+    }
+
+    // // Setter do Sexo
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    // // Getter da Data de Nascimento (retorna int conforme o atributo)
+    public int getDataNascimento() {
+        return dataNascimento;
+    }
+
+    // // Setter da Data de Nascimento
+    public void setDataNascimento(int dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
---
+++ java-intro/src/java03objeto/construtores/ConstrutoresTST.java
@@ -0,0 +1,15 @@
+package java03objeto.construtores;
+
+public class ConstrutoresTST {
+  public static void main(String[] args) {
+    // // Objeto usando o construtor com 3 parâmetros
+    Construtores pessoa = new Construtores("William", 33, 'M');
+
+    // // Objeto usando a sobrecarga com 4 parâmetros
+    Construtores pessoa02 = new Construtores("William", 33, 'M', 1992);
+
+    // // Exibe os dados dos dois objetos
+    pessoa.imprimir();
+    pessoa02.imprimir();
+  }
+}
